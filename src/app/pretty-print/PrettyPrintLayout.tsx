35b78750a2bbca6c4061fabc7c7e6521a3353636
Jonas Lundahl <Jonas.Lundahl@outlook.com>
2024-01-15 21:36:27 +0100

    Add home button

--- src/app/pretty-print/PrettyPrintLayout.tsx
+++ src/app/pretty-print/PrettyPrintLayout.tsx
@@ -1,4 +1,7 @@
 import { PropsWithChildren } from "react";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
+import { HomeIcon } from "@radix-ui/react-icons";
 
 export default function PrettyPrintLayout({
   title,
@@ -6,9 +9,17 @@ export default function PrettyPrintLayout({
 }: PropsWithChildren<{ title: string }>) {
   return (
     <main className="flex flex-col p-4 gap-4 h-screen">
-      <h1 className="self-start scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-center">
-        {title}
-      </h1>
+      <div className="flex justify-between items-center">
+        <h1 className="self-start scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-center">
+          {title}
+        </h1>
+        <Link
+          className={buttonVariants({ variant: "outline", size: "icon" })}
+          href="/"
+        >
+          <HomeIcon className="h-[1.2rem] w-[1.2rem]" />
+        </Link>
+      </div>
       {children}
     </main>
   );
