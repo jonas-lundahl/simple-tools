2e5592e8eeb21909fc9b98a54c84dd20ebf7ea99
Jonas Lundahl <Jonas.Lundahl@outlook.com>
2024-01-15 21:57:51 +0100

    Force dynamic

--- src/app/pretty-print/json/page.tsx
+++ src/app/pretty-print/json/page.tsx
@@ -1,6 +1,8 @@
 import { Metadata } from "next";
 import JSONPrettyPrint from "@/app/pretty-print/json/JSONPrettyPrint";
 
+export const dynamic = "force-dynamic";
+
 export const metadata: Metadata = {
   title: `${process.env.APP_TITLE} | JSON Pretty Print`,
 };
--- src/app/pretty-print/xml/page.tsx
+++ src/app/pretty-print/xml/page.tsx
@@ -1,6 +1,8 @@
 import { Metadata } from "next";
 import XMLPrettyPrint from "@/app/pretty-print/xml/XMLPrettyPrint";
 
+export const dynamic = "force-dynamic";
+
 export const metadata: Metadata = {
   title: `${process.env.APP_TITLE} | XML Pretty Print`,
 };
