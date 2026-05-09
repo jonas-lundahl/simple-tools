fa92dc89457504ce9a9e22fa2ead6aa946f7fe79
Jonas Lundahl <90792675+jonas-lundahl@users.noreply.github.com>
2026-05-09 09:58:20 +0200

    Nitro adjustments & Sentry spans (#4)

--- src/lib/theme.ts
+++ src/lib/theme.ts
@@ -1,15 +1,22 @@
 import { createServerFn } from "@tanstack/react-start";
 import { getCookie, setCookie } from "@tanstack/react-start/server";
 import * as z from "zod";
+import * as Sentry from "@sentry/tanstackstart-react";
 
 const postThemeValidator = z.union([z.literal("light"), z.literal("dark")]);
 export type T = z.infer<typeof postThemeValidator>;
 const storageKey = "_preferred-theme";
 
-export const getThemeServerFn = createServerFn().handler(
-  async () => (getCookie(storageKey) || "dark") as T,
-);
+export const getThemeServerFn = createServerFn().handler(async () => {
+  return Sentry.startSpan({ name: "Getting theme cookie" }, async () => {
+    return (getCookie(storageKey) || "dark") as T;
+  });
+});
 
 export const setThemeServerFn = createServerFn({ method: "POST" })
   .inputValidator(postThemeValidator)
-  .handler(async ({ data }) => setCookie(storageKey, data));
+  .handler(async ({ data }) => {
+    return Sentry.startSpan({ name: "Setting theme cookie" }, async () => {
+      setCookie(storageKey, data);
+    });
+  });
